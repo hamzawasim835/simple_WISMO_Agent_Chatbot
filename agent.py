4c9995146da5e36f7582e2813a2fc877608a7c01
Hamza <hamza.wasim835@gmail.com>
2026-03-08 18:55:46 +0300

    deployment fixes

--- agent.py
+++ agent.py
@@ -2,7 +2,7 @@
 import os
 import random
 from dotenv import load_dotenv
-import langchainhub as hub
+from langchain import hub
 from langchain.agents import AgentExecutor, create_structured_chat_agent
 from langchain.memory import ConversationBufferMemory
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
